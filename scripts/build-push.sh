a3df4545d9a884dee9963974d26eba6cf19d716a
Johan Mora <johan.mora@infometrika.com>
2021-09-21 06:52:16 -0500

    Se actualzian rutas según concordancia de rutas con el front

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -2,14 +2,17 @@
 
 set -e
 
-use_tag="hoagnetwork/media-server:0.2.0"
+use_tag="hoagnetwork/media-server:0.2.2"
 use_dated_tag="${use_tag}-$(date -I)"
+latest="hoagnetwork/media-server:latest"
 
 bash scripts/build.sh
 
-docker tag "$use_tag" "$use_dated_tag"
+docker tag "$use_tag" "$use_dated_tag" 
+docker tag "$use_tag" "$latest"
 
 bash scripts/docker-login.sh
 
 docker push "$use_tag"
 docker push "$use_dated_tag"
+docker push "$latest"
