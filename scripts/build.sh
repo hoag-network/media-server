4552d085c86b035c506255d705b480e9397e136e
Johan Mora <johan.mora@infometrika.com>
2021-11-01 19:54:42 -0500

    Se eliminan scripts innecesarios para CI/CD

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-use_tag="hoagnetwork/media-server:0.2.2"
-
-docker build -t "$use_tag" .
--- scripts/docker-login.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "${{ secrets.DOCKER_PASSWORD }}" | docker login -u "${{ secrets.DOCKER_USERNAME }}" --password-stdin
